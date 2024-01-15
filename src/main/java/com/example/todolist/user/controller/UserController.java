60dc4dc116b2afa9e7830d31d9f87c468866eb0d
Anass CHARKAOUI <119069223+charkaoui007@users.noreply.github.com>
2024-01-15 19:18:35 +0100

    Update UserController.java

--- src/main/java/com/example/todolist/user/controller/UserController.java
+++ src/main/java/com/example/todolist/user/controller/UserController.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/user")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
